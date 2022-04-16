6f5a0051d0b142cf6be75d3319d67eb6c1ccd754
Jeffrey Adorkor <adorkorj@gmail.com>
2022-04-16 19:37:04 +0000

    fixed imports

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import * as React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator, } from 'react-navigation-stack';
-import BottomTabScreen from "./navigation/BottomTab";
-import LoadingScreen from './components/LoadingScreen';
-import NotificationScreen from './screens/Notifications/NotificationScreen';
-import SearchScreen from "./screens/Search/SearchScreen";
-import ViewAllScreen from './screens/ViewAll/ViewAllScreen';
-import SpecialistScreen from './screens/Specialist/SpecialistScreen';
-import TimeSlotScreen from './screens/TimeSlots/TimeSlotsScreen';
-import ConsultaionScreen from './screens/ConsultationDetail/ConsultationDetailScreen';
-import PaymentMethodScreen from './screens/PaymentMethod/PaymentMethodScreen';
-import DoctorProfileScreen from './screens/DoctorProfile/DoctorProfileScreen';
-import ReviewScreen from './screens/Review/ReviewScreen';
-import LabTestAndHealthCheckUpScreen from './screens/LabAndTestCheckup/LabTestAndHealthCheckUpScreen';
-import MessageScreen from './screens/Message/MessageScreen';
-import EditProfileScreen from './screens/EditProfile/EditProfileScreen';
-import PatientDirectoryScreen from './screens/PatientDirectory/PatientDirectoryScreen';
-import AboutUsScreen from './screens/AboutUs/AboutUsScreen';
-import WelcomeScreen from './screens/Auth/WelcomeScreen';
+import BottomTabScreen from "./src/navigation/BottomTab";
+import LoadingScreen from './src/components/LoadingScreen';
+import NotificationScreen from './src/screens/Notifications/NotificationScreen';
+import SearchScreen from "./src/screens/Search/SearchScreen";
+import ViewAllScreen from './src/screens/ViewAll/ViewAllScreen';
+import SpecialistScreen from './src/screens/Specialist/SpecialistScreen';
+import TimeSlotScreen from './src/screens/TimeSlots/TimeSlotsScreen';
+import ConsultaionScreen from './src/screens/ConsultationDetail/ConsultationDetailScreen';
+import PaymentMethodScreen from './src/screens/PaymentMethod/PaymentMethodScreen';
+import DoctorProfileScreen from './src/screens/DoctorProfile/DoctorProfileScreen';
+import ReviewScreen from './src/screens/Review/ReviewScreen';
+import LabTestAndHealthCheckUpScreen from './src/screens/LabAndTestCheckup/LabTestAndHealthCheckUpScreen';
+import MessageScreen from './src/screens/Message/MessageScreen';
+import EditProfileScreen from './src/screens/EditProfile/EditProfileScreen';
+import PatientDirectoryScreen from './src/screens/PatientDirectory/PatientDirectoryScreen';
+import AboutUsScreen from './src/screens/AboutUs/AboutUsScreen';
+import WelcomeScreen from './src/screens/Auth/WelcomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import RegisterScreen from './screens/Auth/RegisterScreen';
-import VerificationScreen from './screens/Auth/VerificationScreen';
-import SplashScreen from './screens/SplashScreen';
+import RegisterScreen from './src/screens/Auth/RegisterScreen';
+import VerificationScreen from './src/screens/Auth/VerificationScreen';
+import SplashScreen from './src/screens/SplashScreen';
 
 const navigator = createAppContainer(
   createSwitchNavigator({
